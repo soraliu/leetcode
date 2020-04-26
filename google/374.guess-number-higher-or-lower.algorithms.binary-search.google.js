56e16cfdc0b583291da297d2d82d2a685f44c1fb
Sora Liu <soraliu.dev@gmail.com>
2020-04-26 17:19:11 +0800

    feat(algorithm): leetcode submit 374.guess-number-higher-or-lower.algorithms.binary-search.google.js   ✘ Time Limit Exceeded   ✘ 1/25 cases passed (N/A)   ✘ Testcase: 1 1   ✘ Answer:   ✘ Expected Answer:   ✘ Stdout:

---
+++ google/374.guess-number-higher-or-lower.algorithms.binary-search.google.js
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=374 lang=javascript
+ *
+ * [374] 猜数字大小
+ *
+ * https://leetcode-cn.com/problems/guess-number-higher-or-lower/description/
+ *
+ * algorithms
+ * Easy (43.79%)
+ * Likes:    58
+ * Dislikes: 0
+ * Total Accepted:    21.1K
+ * Total Submissions: 48.1K
+ * Testcase Example:  '10\n6'
+ *
+ * 我们正在玩一个猜数字游戏。 游戏规则如下：
+ * 我从 1 到 n 选择一个数字。 你需要猜我选择了哪个数字。
+ * 每次你猜错了，我会告诉你这个数字是大了还是小了。
+ * 你调用一个预先定义好的接口 guess(int num)，它会返回 3 个可能的结果（-1，1 或 0）：
+ * 
+ * -1 : 我的数字比较小
+ * ⁠1 : 我的数字比较大
+ * ⁠0 : 恭喜！你猜对了！
+ * 
+ * 
+ * 示例 :
+ * 
+ * 输入: n = 10, pick = 6
+ * 输出: 6
+ * 
+ */
+
+// @lc code=start
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	            -1 if num is lower than the guess number
+ *			             1 if num is higher than the guess number
+ *                       otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function(n) {
+  pick = Math.floor(n / 2)
+
+  let judge = guess(pick)
+  while (judge !== 0) {
+    pick = judge < 0 ? Math.floor(pick / 2) : Math.floor((pick + n) / 2)
+    judge = guess(pick)
+  }
+
+  return pick
+};
+// @lc code=end
