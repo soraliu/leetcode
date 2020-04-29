c294a46849345b0f70d65f48933dd4dede424a9a
Sora Liu <soraliu.dev@gmail.com>
2020-04-29 08:40:36 +0800

    feat(algorithm): leetcode submit 62.unique-paths.algorithms.array.bloomberg.dynamic-programming.1.js   ✔ Accepted   ✔ 62/62 cases passed (100 ms)   ✔ Your runtime beats 5.75 % of javascript submissions   ✔ Your memory usage beats 33.33 % of javascript submissions (34.9 MB)

---
+++ src/62.unique-paths.algorithms.array.bloomberg.dynamic-programming.1.js
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=javascript
+ *
+ * [62] 不同路径
+ *
+ * https://leetcode-cn.com/problems/unique-paths/description/
+ *
+ * algorithms
+ * Medium (60.01%)
+ * Likes:    508
+ * Dislikes: 0
+ * Total Accepted:    96.8K
+ * Total Submissions: 161K
+ * Testcase Example:  '3\n2'
+ *
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+ * 
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+ * 
+ * 问总共有多少条不同的路径？
+ * 
+ * 
+ * 
+ * 例如，上图是一个7 x 3 的网格。有多少可能的路径？
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: m = 3, n = 2
+ * 输出: 3
+ * 解释:
+ * 从左上角开始，总共有 3 条路径可以到达右下角。
+ * 1. 向右 -> 向右 -> 向下
+ * 2. 向右 -> 向下 -> 向右
+ * 3. 向下 -> 向右 -> 向右
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: m = 7, n = 3
+ * 输出: 28
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= m, n <= 100
+ * 题目数据保证答案小于等于 2 * 10 ^ 9
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+  let dp = new Array(n)
+  for (let i = 0, len = n; i < len; i++) {
+    if (!dp[i]) dp[i] = new Array(m).fill(0)
+    for (let j = 0, len = m; j < len; j++) {
+      if (i === 0 && j === 0) {
+        dp[i][j] = 1
+        continue
+      }
+
+      if (i - 1 >= 0) dp[i][j] += dp[i - 1][j]
+      if (j - 1 >= 0) dp[i][j] += dp[i][j - 1]
+    }
+  }
+  return dp[n - 1][m - 1]
+};
+// @lc code=end
