37d5efdeea4980609f57a7a236df1790c6095921
Sora Liu <soraliu.dev@gmail.com>
2020-04-29 08:48:06 +0800

    feat(algorithm): leetcode submit 63.unique-paths-ii.algorithms.array.bloomberg.dynamic-programming.1.js   ✘ Wrong Answer   ✘ 18/43 cases passed (N/A)   ✘ Testcase: [[0,0],[1,1],[0,0]]   ✘ Answer: 1   ✘ Expected Answer: 0   ✘ Stdout:

--- src/63.unique-paths-ii.algorithms.array.bloomberg.dynamic-programming.1.js
+++ src/63.unique-paths-ii.algorithms.array.bloomberg.dynamic-programming.1.js
@@ -52,12 +52,14 @@ var uniquePathsWithObstacles = function(obstacleGrid) {
   let m = obstacleGrid.length
   let n = obstacleGrid[0].length
 
+  if (obstacleGrid[0][0] || obstacleGrid[m - 1][n - 1]) return 0
+
   let dp = new Array()
   for (let i = 0, len = n; i < len; i++) {
     if (!dp[i]) dp[i] = new Array(m).fill(0)
     for (let j = 0, len = m; j < len; j++) {
       if (i === 0 && j === 0) {
-        dp[i][j] = obstacleGrid[0][0] === 1 ? 0 : 1
+        dp[i][j] = 1
         continue
       }
 
