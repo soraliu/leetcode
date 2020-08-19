8f604b9d5532129df082a5b52fca97f876f81418
Sora Liu <soraliu.dev@gmail.com>
2020-08-19 11:24:18 +0800

    feat(algorithm): leetcode submit 572.subtree-of-another-tree.algorithms.ebay.facebook.google.tree.1.js
    duration: 0:37:18
    
      ✘ Wrong Answer
      ✘ 107/183 cases passed (N/A)
      ✘ Testcase: [3,4,5,1,2,null,null,0]
    [4,1,2]
      ✘ Answer: undefined
      ✘ Expected Answer: false
      ✘ Stdout:

---
+++ src/572.subtree-of-another-tree.algorithms.ebay.facebook.google.tree.1.js
@@ -0,0 +1,101 @@
+/*
+ * @lc app=leetcode.cn id=572 lang=javascript
+ *
+ * [572] 另一个树的子树
+ *
+ * https://leetcode-cn.com/problems/subtree-of-another-tree/description/
+ *
+ * algorithms
+ * Easy (44.48%)
+ * Likes:    339
+ * Dislikes: 0
+ * Total Accepted:    47.2K
+ * Total Submissions: 100.6K
+ * Testcase Example:  '[3,4,5,1,2]\n[4,1,2]'
+ *
+ * 给定两个非空二叉树 s 和 t，检验s 中是否包含和 t 具有相同结构和节点值的子树。s 的一个子树包括 s 的一个节点和这个节点的所有子孙。s
+ * 也可以看做它自身的一棵子树。
+ *
+ * 示例 1:
+ * 给定的树 s:
+ *
+ *
+ * ⁠    3
+ * ⁠   / \
+ * ⁠  4   5
+ * ⁠ / \
+ * ⁠1   2
+ *
+ *
+ * 给定的树 t：
+ *
+ *
+ * ⁠  4
+ * ⁠ / \
+ * ⁠1   2
+ *
+ *
+ * 返回 true，因为 t 与 s 的一个子树拥有相同的结构和节点值。
+ *
+ * 示例 2:
+ * 给定的树 s：
+ *
+ *
+ * ⁠    3
+ * ⁠   / \
+ * ⁠  4   5
+ * ⁠ / \
+ * ⁠1   2
+ * ⁠   /
+ * ⁠  0
+ *
+ *
+ * 给定的树 t：
+ *
+ *
+ * ⁠  4
+ * ⁠ / \
+ * ⁠1   2
+ *
+ *
+ * 返回 false。
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} s
+ * @param {TreeNode} t
+ * @return {boolean}
+ */
+var isSubtree = function (s, t) {
+  /**
+   * @return {boolean}
+   */
+  const compare = (root1, root2) => {
+    if (root1 === null && root2 === null) return true
+    if (root1 === null || root2 === null) return false
+
+    return root1.val === root2.val && compare(root1.left, root2.left) && compare(root1.right, root2.right)
+  }
+
+  const dfs = [s]
+  while (dfs.length > 0) {
+    const node = dfs.pop()
+
+    if (node.val === t.val && compare(node, t)) return true
+
+    if (node.right) dfs.push(node.right)
+    if (node.left) dfs.push(node.left)
+  }
+}
+// @lc code=end
+module.exports = isSubtree
