6bce3b62ffebe7ac2ddc3b771ca77817ebfd1073
Sora Liu <soraliu.dev@gmail.com>
2020-08-19 11:52:38 +0800

    feat(algorithm): leetcode submit 530.minimum-absolute-difference-in-bst.algorithms.array.binary-search-tree.depth-first-search.google.3.js
    duration: 0:15:20
    
      ✘ Wrong Answer
      ✘ 32/186 cases passed (N/A)
      ✘ Testcase: [543,384,652,null,445,null,699]
      ✘ Answer: 61
      ✘ Expected Answer: 47
      ✘ Stdout:

---
+++ src/530.minimum-absolute-difference-in-bst.algorithms.array.binary-search-tree.depth-first-search.google.3.js
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode.cn id=530 lang=javascript
+ *
+ * [530] 二叉搜索树的最小绝对差
+ *
+ * https://leetcode-cn.com/problems/minimum-absolute-difference-in-bst/description/
+ *
+ * algorithms
+ * Easy (56.80%)
+ * Likes:    128
+ * Dislikes: 0
+ * Total Accepted:    18.9K
+ * Total Submissions: 32.8K
+ * Testcase Example:  '[1,null,3,2]'
+ *
+ * 给你一棵所有节点为非负值的二叉搜索树，请你计算树中任意两节点的差的绝对值的最小值。
+ *
+ *
+ *
+ * 示例：
+ *
+ * 输入：
+ *
+ * ⁠  1
+ * ⁠   \
+ * ⁠    3
+ * ⁠   /
+ * ⁠  2
+ *
+ * 输出：
+ * 1
+ *
+ * 解释：
+ * 最小绝对差为 1，其中 2 和 1 的差的绝对值为 1（或者 2 和 3）。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中至少有 2 个节点。
+ * 本题与 783 https://leetcode-cn.com/problems/minimum-distance-between-bst-nodes/
+ * 相同
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var getMinimumDifference = function (root) {
+  const inorder = []
+  let current = root
+
+  let prev = null
+  let min = Infinity
+
+  while (inorder.length || current) {
+    if (current !== null) {
+      inorder.push(current)
+      current = current.left
+    } else {
+      current = inorder.pop()
+
+      if (!prev) {
+        prev = current.val
+      } else {
+        min = Math.min(min, current.val - prev)
+      }
+
+      current = current.right
+    }
+  }
+
+  return min
+}
+// @lc code=end
+module.exports = getMinimumDifference
