7b205ad211e910794e848966e030cb1bb1141d16
Sora Liu <soraliu.dev@gmail.com>
2020-09-01 11:00:19 +0800

    feat(algorithm): leetcode submit 665.non-decreasing-array.algorithms.array.google.1.js
    duration: 0:49:26
    
      ✘ Wrong Answer
      ✘ 308/334 cases passed (N/A)
      ✘ Testcase: [4,2,1]
      ✘ Answer: true
      ✘ Expected Answer: false
      ✘ Stdout:

--- src/665.non-decreasing-array.algorithms.array.google.1.js
+++ src/665.non-decreasing-array.algorithms.array.google.1.js
@@ -56,10 +56,17 @@ var checkPossibility = function (nums) {
   let met = false
   for (let i = 0, len = nums.length - 1; i < len; i++) {
     if (nums[i] > nums[i + 1]) {
-      if (met || (i > 0 && nums[i - 1] > nums[i + 1])) {
-        return false
-      } else {
+      if (
+        !met &&
+        (
+          (i === 0 || nums[i - 1] < nums[i + 1]) ||
+          (i === len - 1 || nums[i] < nums[i + 2])
+        )
+      ) {
+        i++
         met = true
+      } else {
+        return false
       }
     }
   }
