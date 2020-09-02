227f2194ea9353027a631fc77e96078cf630526e
Sora Liu <soraliu.dev@gmail.com>
2020-09-02 09:45:36 +0800

    feat(algorithm): leetcode submit 686.repeated-string-match.algorithms.google.string.1.js
    duration: 0:17:20
    
      ✘ Runtime Error
      ✘ 3/56 cases passed (N/A)
      ✘ Error: Line ?: ?
      ✘ Error: FATAL ERROR: CALL_AND_RETRY_LAST Allocation failed - JavaScript heap out of memory
     1: 0xa2afd0 node::Abort() [nodejs run]
     2: 0x97a467 node::FatalError(char const*, char const*) [nodejs run]
     3: 0xb9e04e v8::Utils::ReportOOMFailure(v8::internal::Isolate*, char const*, bool) [nodejs run]
     4: 0xb9e3c7 v8::internal::V8::FatalProcessOutOfMemory(v8::internal::Isolate*, char const*, bool) [nodejs run]
     5: 0xd3e7d5  [nodejs run]
     6: 0xd50830 v8::internal::Heap::AllocateRawWithRetryOrFailSlowPath(int, v8::internal::AllocationType, v8::internal::AllocationOrigin, v8::internal::AllocationAlignment) [nodejs run]
     7: 0xd15dea v8::internal::Factory::AllocateRaw(int, v8::internal::AllocationType, v8::internal::AllocationAlignment) [nodejs run]
     8: 0xd13684 v8::internal::FactoryBase<v8::internal::Factory>::AllocateRawWithImmortalMap(int, v8::internal::AllocationType, v8::internal::Map, v8::internal::AllocationAlignment) [nodejs run]
     9: 0xd13b90 v8::internal::FactoryBase<v8::internal::Factory>::NewRawOneByteString(int, v8::internal::AllocationType) [nodejs run]
    10: 0xf5ec6a v8::internal::String::SlowFlatten(v8::internal::Isolate*, v8::internal::Handle<v8::internal::ConsString>, v8::internal::AllocationType) [nodejs run]
    11: 0xf5f400 v8::internal::String::IndexOf(v8::internal::Isolate*, v8::internal::Handle<v8::internal::String>, v8::internal::Handle<v8::internal::String>, int) [nodejs run]
    12: 0x108bdab v8::internal::Runtime_StringIndexOfUnchecked(int, unsigned long*, v8::internal::Isolate*) [nodejs run]
    13: 0x13a9e39  [nodejs run]
      ✘ Testcase: "abc"
    "wxyz"
      ✘ Answer:
      ✘ Expected Answer:
      ✘ Stdout:
    <--- Last few GCs --->
    
    [44:0x469b560]      441 ms: Mark-sweep 194.8 (203.8) -> 194.8 (196.8) MB, 4.4 / 0.0 ms  (average mu = 0.922, current mu = 0.052) last resort GC in old space requested
    [44:0x469b560]      445 ms: Mark-sweep 194.8 (196.8) -> 194.7 (196.8) MB, 3.6 / 0.0 ms  (average mu = 0.898, current mu = 0.003) last resort GC in old space requested
    
    <--- JS stacktrace --->

---
+++ src/686.repeated-string-match.algorithms.google.string.1.js
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=686 lang=javascript
+ *
+ * [686] 重复叠加字符串匹配
+ *
+ * https://leetcode-cn.com/problems/repeated-string-match/description/
+ *
+ * algorithms
+ * Easy (33.87%)
+ * Likes:    104
+ * Dislikes: 0
+ * Total Accepted:    12.4K
+ * Total Submissions: 36.5K
+ * Testcase Example:  '"abcd"\n"cdabcdab"'
+ *
+ * 给定两个字符串 A 和 B, 寻找重复叠加字符串A的最小次数，使得字符串B成为叠加后的字符串A的子串，如果不存在则返回 -1。
+ *
+ * 举个例子，A = "abcd"，B = "cdabcdab"。
+ *
+ * 答案为 3，因为 A 重复叠加三遍后为“abcdabcdabcd”，此时 B 是其子串；A 重复叠加两遍后为"abcdabcd"，B
+ * 并不是其子串。
+ *
+ * 注意:
+ *
+ * A与B字符串的长度在1和10000区间范围内。
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {number}
+ */
+var repeatedStringMatch = function (a, b) {
+  let count = 1
+
+  while (true) {
+    if (a.indexOf(b) < 0) {
+      if (b < (a * (count - 1))) {
+        return -1
+      }
+
+      a += a
+      count++
+    } else {
+      return count
+    }
+  }
+}
+// @lc code=end
+module.exports = repeatedStringMatch
