2f00744e11490754f4495db0613a57ccf594aa93
Sora Liu <soraliu.dev@gmail.com>
2020-10-16 11:06:01 +0800

    feat(algorithm): leetcode submit 139.word-break.algorithms.amazon.bloomberg.dynamic-programming.facebook.google.pocketgems.uber.yahoo.1.js
    duration: 0:9:22
    
      ✘ Time Limit Exceeded
      ✘ 36/43 cases passed (N/A)
      ✘ Testcase: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"
    ' +
      '["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
      ✘ Answer:
      ✘ Expected Answer:
      ✘ Stdout:

---
+++ src/139.word-break.algorithms.amazon.bloomberg.dynamic-programming.facebook.google.pocketgems.uber.yahoo.1.js
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode.cn id=139 lang=javascript
+ *
+ * [139] 单词拆分
+ *
+ * https://leetcode-cn.com/problems/word-break/description/
+ *
+ * algorithms
+ * Medium (44.50%)
+ * Likes:    714
+ * Dislikes: 0
+ * Total Accepted:    95.3K
+ * Total Submissions: 199K
+ * Testcase Example:  '"leetcode"\n["leet","code"]'
+ *
+ * 给定一个非空字符串 s 和一个包含非空单词的列表 wordDict，判定s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+ *
+ * 说明：
+ *
+ *
+ * 拆分时可以重复使用字典中的单词。
+ * 你可以假设字典中没有重复的单词。
+ *
+ *
+ * 示例 1：
+ *
+ * 输入: s = "leetcode", wordDict = ["leet", "code"]
+ * 输出: true
+ * 解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+ *
+ *
+ * 示例 2：
+ *
+ * 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+ * 输出: true
+ * 解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
+ * 注意你可以重复使用字典中的单词。
+ *
+ *
+ * 示例 3：
+ *
+ * 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * 输出: false
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function (s, wordDict) {
+  let ans = false
+
+  const dp = target => {
+    if (target.length === 0) {
+      ans = true
+      return
+    }
+
+    wordDict.forEach(word => {
+      if (target.slice(0, word.length) === word) {
+        dp(target.slice(word.length))
+      }
+    })
+  }
+
+  dp(s)
+
+  return ans
+}
+// @lc code=end
+module.exports = wordBreak
