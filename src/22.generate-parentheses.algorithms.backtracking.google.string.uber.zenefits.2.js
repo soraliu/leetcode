7326cd8cc5400a304305fb4d345f1730cc123de8
Sora Liu <soraliu.dev@gmail.com>
2020-10-20 14:54:23 +0800

    feat(algorithm): leetcode submit 22.generate-parentheses.algorithms.backtracking.google.string.uber.zenefits.2.js
    duration: 0:12:7
    
      ✔ Accepted
      ✔ 8/8 cases passed (76 ms)
      ✔ Your runtime beats 92.49 % of javascript submissions
      ✔ Your memory usage beats 10.88 % of javascript submissions (39.6 MB)

---
+++ src/22.generate-parentheses.algorithms.backtracking.google.string.uber.zenefits.2.js
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=22 lang=javascript
+ *
+ * [22] 括号生成
+ *
+ * https://leetcode-cn.com/problems/generate-parentheses/description/
+ *
+ * algorithms
+ * Medium (75.26%)
+ * Likes:    981
+ * Dislikes: 0
+ * Total Accepted:    119.7K
+ * Total Submissions: 159K
+ * Testcase Example:  '3'
+ *
+ * 数字 n代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ *
+ *
+ *
+ * 示例：
+ *
+ * 输入：n = 3
+ * 输出：[
+ * ⁠      "((()))",
+ * ⁠      "(()())",
+ * ⁠      "(())()",
+ * ⁠      "()(())",
+ * ⁠      "()()()"
+ * ⁠    ]
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function (n) {
+  const ans = []
+
+  const dfs = ({ str, left, right }) => {
+    if (left > right || left < 0 || right < 0) return
+
+    if (left === 0 && right === 0) {
+      ans.push(str)
+    }
+
+    dfs({ str: `${str}(`, left: left - 1, right })
+    dfs({ str: `${str})`, left: left, right: right - 1 })
+  }
+
+  dfs({ str: '', left: n, right: n })
+
+  return ans
+}
+// @lc code=end
+console.log(generateParenthesis(4))
+module.exports = generateParenthesis
