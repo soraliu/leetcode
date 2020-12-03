c3066d694a7fe6b1f00ea1e4262fc37970c1fd05
Sora Liu <soraliu.dev@gmail.com>
2020-12-03 20:19:07 +0800

    feat(algorithm): leetcode submit 42.trapping-rain-water.algorithms.amazon.apple.array.bloomberg.google.stack.twitter.two-pointers.zenefits.1.js
    duration: 0:28:25
    
      ✘ Wrong Answer
      ✘ 31/320 cases passed (N/A)
      ✘ Testcase: [4,2,0,3,2,5]
      ✘ Answer: 4
      ✘ Expected Answer: 9
      ✘ Stdout:

---
+++ src/42.trapping-rain-water.algorithms.amazon.apple.array.bloomberg.google.stack.twitter.two-pointers.zenefits.1.js
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode.cn id=42 lang=javascript
+ *
+ * [42] 接雨水
+ *
+ * https://leetcode-cn.com/problems/trapping-rain-water/description/
+ *
+ * algorithms
+ * Hard (53.30%)
+ * Likes:    1843
+ * Dislikes: 0
+ * Total Accepted:    170.6K
+ * Total Submissions: 319.7K
+ * Testcase Example:  '[0,1,0,2,1,0,1,3,2,1,2,1]'
+ *
+ * 给定n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ *
+ * 输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 输出：6
+ * 解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：height = [4,2,0,3,2,5]
+ * 输出：9
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * n == height.length
+ * 0
+ * 0
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function (height) {
+  let ret = 0
+
+  const maxHeightOfLeft = []
+  const maxHeightOfRight = []
+
+  maxHeightOfLeft[0] = height[0]
+  maxHeightOfRight[height.length - 1] = height[height.length - 1]
+
+  for (let i = 1, len = height.length; i < len; i++) {
+    maxHeightOfLeft[i] = height[i] >= maxHeightOfLeft[i - 1] ? height[i] : maxHeightOfLeft[i - 1]
+    maxHeightOfRight[len - 1 - i] = height[len - 1 - i] >= maxHeightOfRight[len - i] ? height[len - 1 - i] : maxHeightOfRight[len - i]
+  }
+
+  for (let i = 1, len = height.length - 1; i < len; i++) {
+    if (maxHeightOfLeft[i - 1] > height[i] && height[i] < maxHeightOfRight[i + 1]) {
+      ret += Math.abs(maxHeightOfLeft[i - 1] - maxHeightOfRight[i + 1])
+    }
+  }
+
+  return ret
+}
+// @lc code=end
+module.exports = trap
