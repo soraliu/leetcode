d732334653b1cff741f3f5c315075f51b73fbbf5
Sora Liu <soraliu.dev@gmail.com>
2022-06-12 18:50:41 +0800

    feat(algorithm): leetcode submit 5.longest-palindromic-substring.algorithms.amazon.bloomberg.dynamic-programming.microsoft.string.2.js
    duration: 0:37:20
    
      ✘ Time Limit Exceeded
      ✘ 47/180 cases passed (N/A)
      ✘ Testcase: "ibvjkmpyzsifuxcabqqpahjdeuzaybqsrsmbfplxycsafogotliyvhxjtkrbzqxlyfwujzhkdafhebvsdhkkdbhlhmaoxmbkqiwiusngkbdhlvxdyvnjrzvxmukvdfobzlmvnbnilnsyrgoygfdzjlymhprcpxsnxpcafctikxxybcusgjwmfklkffehbvlhvxfiddznwumxosomfbgxoruoqrhezgsgidgcfzbtdftjxeahriirqgxbhicoxavquhbkaomrroghdnfkknyigsluqebaqrtcwgmlnvmxoagisdmsokeznjsnwpxygjjptvyjjkbmkxvlivinmpnpxgmmorkasebngirckqcawgevljplkkgextudqaodwqmfljljhrujoerycoojwwgtklypicgkyaboqjfivbeqdlonxeidgxsyzugkntoevwfuxovazcyayvwbcqswzhytlmtmrtwpikgacnpkbwgfmpavzyjoxughwhvlsxsgttbcyrlkaarngeoaldsdtjncivhcfsaohmdhgbwkuemcembmlwbwquxfaiukoqvzmgoeppieztdacvwngbkcxknbytvztodbfnjhbtwpjlzuajnlzfmmujhcggpdcwdquutdiubgcvnxvgspmfumeqrofewynizvynavjzkbpkuxxvkjujectdyfwygnfsukvzflcuxxzvxzravzznpxttduajhbsyiywpqunnarabcroljwcbdydagachbobkcvudkoddldaucwruobfylfhyvjuynjrosxczgjwudpxaqwnboxgxybnngxxhibesiaxkicinikzzmonftqkcudlzfzutplbycejmkpxcygsafzkgudy"
      ✘ Answer:
      ✘ Expected Answer: "fklkf"
      ✘ Stdout:

---
+++ src/5.longest-palindromic-substring.algorithms.amazon.bloomberg.dynamic-programming.microsoft.string.2.js
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=5 lang=javascript
+ *
+ * [5] Longest Palindromic Substring
+ *
+ * https://leetcode.com/problems/longest-palindromic-substring/description/
+ *
+ * algorithms
+ * Medium (31.75%)
+ * Likes:    18187
+ * Dislikes: 1077
+ * Total Accepted:    1.9M
+ * Total Submissions: 5.9M
+ * Testcase Example:  '"babad"'
+ *
+ * Given a string s, return the longest palindromic substring in s.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: s = "babad"
+ * Output: "bab"
+ * Explanation: "aba" is also a valid answer.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: s = "cbbd"
+ * Output: "bb"
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= s.length <= 1000
+ * s consist of only digits and English letters.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var getLongestRightPalindrome = function (s) {
+  for (let i = 0, len = s.length; i < len; i++) {
+    const substr = s.substring(i, len)
+    if (substr === substr.split('').reverse().join('')) {
+      return substr
+    }
+  }
+}
+
+var longestPalindrome = function (s) {
+  if (s.length === 1) {
+    return s
+  }
+
+  const longestRightPalindrome = getLongestRightPalindrome(s)
+  const longestPrevPalindrome = longestPalindrome(s.substr(0, s.length - 1))
+
+  return longestRightPalindrome.length >= longestPrevPalindrome.length ? longestRightPalindrome : longestPrevPalindrome
+}
+
+// @lc code=end
+module.exports = longestPalindrome
